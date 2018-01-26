a9f239a813bb52ecb15d82b8604045534539084e
Harleigh Abel <harleighabel@gmail.com>
2018-01-26 22:53:15 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,10 +14,10 @@ def add_element_to_start_of_array(array, element)
   array.unshift(element)
 end
 
-def remove_element_from_end_of_array(array, element)
+def remove_element_from_end_of_array(array)
   array.pop(element)
-end  
+end
 
-def remove_element_from_start_of_array(array, element)
+def remove_element_from_start_of_array(array)
   array.shift(element)
-end  
+end
